ec2af3637f0a8d237678b72cbefe8366720b900b
akbare <akbaremonz@gmail.com>
2024-07-24 17:08:36 +0700

    basic

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title></title>
+</head>
+<link rel="stylesheet" href="style.css" type="text/css">
+<body>
+	<tr valign="top">
+		<td>
+		<div style="height:293px; width:100%;">
+		<table border="0" width="100%" cellspacing="4" cellpadding="4">
+			<tr>
+				<td width="100%"><span class="header">Welcome to DEF-ROnline!</span>
+					<div class="content">
+						<ul>
+
+						<br>
+<li>Classic Server</li>
+<li>Pre-Renewal</li>
+						</ul>
+					</div>
+				</td>
+				</tr>
+		</table>
+		</div>
+		</td>
+	</tr>
+</body>
+</html>
